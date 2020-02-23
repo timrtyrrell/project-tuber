5835726e33285baf930600d3f5f81839eeeffce7
Manana Metreveli <mananametreveli@MacBook-Pro.local>
2020-02-23 11:54:43 -0500

    Added Bootstrap to request_help feature

--- request_help/views.py
+++ request_help/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+from .models import HelpRequest
+
+# Request Help
 def request_help(request):
-    return render(request, 'request_help/request.html', {})
+    if request.method == 'GET': 
+        return render(request, 'request_help/request.html', {})
+    elif request.method == 'POST':
+        help_request = HelpRequest.objects.create(class_name=request.POST['class_name'], topic_text=request.POST['topic_text'])
+    return HttpResponseRedirect(reverse('request_help:request'))
