56a3cc5f1ffac9ef8f75a04f7b3dfcb9fdd41799
Manana Metreveli <mananametreveli@d-172-25-99-16.dhcp.virginia.edu>
2020-02-27 14:56:38 -0500

    Added a locations field to RequestHelp model

--- request_help/models.py
+++ request_help/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class HelpRequest(models.Model):
     class_name = models.CharField(max_length=20)
     topic_text = models.CharField(max_length=200)
+    location = models.CharField(max_length=100, default="Rice Hall")
 
     def __str__(self):
         return self.topic_text
--- request_help/views.py
+++ request_help/views.py
@@ -9,7 +9,7 @@ def request_help(request):
     if request.method == 'GET': 
         return render(request, 'request_help/request.html', {})
     if request.method == 'POST':
-        help_request = HelpRequest.objects.create(class_name=request.POST['class_name'], topic_text=request.POST['topic_text'])
+        help_request = HelpRequest.objects.create(class_name=request.POST['class_name'], topic_text=request.POST['topic_text'], location=request.POST['location'])
     return HttpResponseRedirect(reverse('sending_help'))
 
 # Help request received
