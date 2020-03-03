b6f5de4ec2053051d5b27025a051b42d26f02f7d
Manana Metreveli <mananametreveli@MacBook-Pro.local>
2020-03-03 13:42:55 -0500

    Styled location dropdown

--- request_help/forms.py
+++ request_help/forms.py
@@ -1,7 +1,11 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Select
 from .models import HelpRequest
 
 class HelpRequestForm(ModelForm):
     class Meta:
         model = HelpRequest
         fields = ['class_name', 'topic', 'location']
+        widgets = {
+            'location': Select(attrs={'class': 'form-control'}),
+        }
+    
