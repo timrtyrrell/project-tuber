cd05be695911d270251b028e009964a66eec2cdf
Nick <figginsn@gmail.com>
2020-03-31 11:12:08 -0400

    Fixed userprofile

--- register/models.py
+++ register/models.py
@@ -9,6 +9,6 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=10)
     name = models.CharField(max_length=100)
-    status = models.BooleanField(choices=BOOL_CHOICES)
+    status = models.BooleanField(choices=BOOL_CHOICES, default=False)
     status_string = models.CharField(max_length=30, default="Not Available")
     tutor_location = models.CharField(max_length=20, default="Anywhere")
