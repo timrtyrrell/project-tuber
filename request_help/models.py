bb1f0710fc6ad4e0336f0974735a481e4fc7f5bd
Manana Metreveli <mananametreveli@MacBook-Pro.fios-router.home>
2020-04-06 12:27:28 -0400

    Styled Current Requests page

--- request_help/models.py
+++ request_help/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
 LOCATIONS = (
-    ('clem','Clemons Library'),
-    ('aldy', 'Alderman Library'),
-    ('clark','Clark Library'),
-    ('music','Music Library'),
-    ('rice','Rice Hall'),
+    ('Clemons Library','Clemons Library'),
+    ('Alderman Library', 'Alderman Library'),
+    ('Clark Library','Clark Library'),
+    ('Music Library','Music Library'),
+    ('Rice Hall','Rice Hall'),
 )
 
 
 class HelpRequest(models.Model):
     class_name = models.CharField(max_length=20)
     topic = models.CharField(max_length=200)
-    location = models.CharField(max_length=6, choices=LOCATIONS, default='green')
+    location = models.CharField(max_length=60, choices=LOCATIONS, default='green')
     # need to attach a user here for contact information
 
     def __str__(self):
