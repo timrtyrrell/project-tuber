a3696d208c6102d7a7e7e91cc42602108a3af3c0
Nick <figginsn@gmail.com>
2020-04-05 20:30:28 -0400

    Added basic tests for tutorStatus and register page

--- tuber/tests.py
+++ tuber/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from request_help.models import HelpRequest
+from register.models import UserProfile
 
 class BasicTests(TestCase):
     def test_isTrue(self):
@@ -19,3 +20,25 @@ class helpRequestTests(TestCase):
     def test_isClassName(self):
         current_request = HelpRequest(class_name = "CS3240", topic = "I need help", location = "Clem Library")
         self.assertEqual(current_request.class_name, "CS3240")
+
+class tutorStatusTests(TestCase): 
+    def test_isDefaultStatus(self):
+        current_tutor = UserProfile(name="Bob", phone="434-123-4567")
+        self.assertEqual(current_tutor.status, False)
+    def test_isDefaultLocation(self):
+        current_tutor = UserProfile(name="Bob", phone="434-123-4567")
+        self.assertEqual(current_tutor.tutor_location, "Anywhere")
+    def test_isNewLocation(self):
+        current_tutor = UserProfile(name = "Bobby", phone="123-434-3567", tutor_location = "Clem Library")
+        self.assertEqual(current_tutor.tutor_location, "Clem Library")
+
+class registerTests(TestCase): 
+    def test_isDefault(self):
+        current_user = UserProfile()
+        self.assertEqual(current_user.name, "")
+    def test_isPhone(self):
+        current_user = UserProfile(name="Bob", phone="434-123-4567")
+        self.assertEqual(current_user.phone, "434-123-4567")
+    def test_isName(self):
+        current_user = UserProfile(name = "Bobby", phone="123-434-3567")
+        self.assertEqual(current_user.name, "Bobby")
