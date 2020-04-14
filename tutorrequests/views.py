756bb251968888da5230e6ab64c6ad374c7a5a6c
Nick <figginsn@gmail.com>
2020-04-14 13:32:09 -0400

    Fixed lat/long error on contact student

--- tutorrequests/views.py
+++ tutorrequests/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from request_help.models import HelpRequest
 # import googlemaps
 from datetime import datetime
-
+import googlemaps
 from tuber.settings import GOOGLE_MAPS_API_KEY
 
 
@@ -17,19 +17,20 @@ def details(request, pk):
     req = HelpRequest.objects.get(pk=pk)
     
     loc = str(req.location)
-
+    print(loc)
     locations = {
         'clem': '164 McCormick Rd, Charlottesville, VA 22903',
-        'clark': '291 McCormick Rd, Charlottesville, VA 22903',
-        'aldy': '160 McCormick Rd, Charlottesville, VA 22904',
-        'music': 'Cabell Dr L001, Charlottesville, VA 22904',
-        'rice': '5 Engineer\'s Way, Charlottesville, VA 22903',
+        'Clark Library': '291 McCormick Rd, Charlottesville, VA 22903',
+        'Alderman Library': '160 McCormick Rd, Charlottesville, VA 22904',
+        'Music Library': 'Cabell Dr L001, Charlottesville, VA 22904',
+        'Rice Hall': '5 Engineer\'s Way, Charlottesville, VA 22903',
     }
 
     gmaps = googlemaps.Client(key=GOOGLE_MAPS_API_KEY)
     student_location = locations.get(loc)
-    tutor_location = locations.get('aldy')
+    tutor_location = locations.get('Alderman Library')
     now = datetime.now()
+    print(student_location, tutor_location)
     directions_result = gmaps.directions(student_location, tutor_location, mode="walking", departure_time="now")
     walking = directions_result[0]['legs']
     directions_distance = walking[0]['distance']
